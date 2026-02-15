d3b1e4d08ca552ef94dabb0fa447c5f5097cdb26
2bo <developer.2bo@gmail.com>
2026-02-15 18:01:48 +0900

    refactor(slack): derive action regex from action id prefixes Entire-Checkpoint: 007fd25de0ab

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config';
 
 import { initSlackApp, startSlackApp } from './slack/bolt-app';
+import { ACTION_ID_PREFIXES } from './slack/constants';
 import { handleAction } from './slack/handlers/action-handler';
 import { handleMention } from './slack/handlers/mention-handler';
+
+const APPROVE_ACTION_RE = new RegExp(`^${ACTION_ID_PREFIXES.APPROVE}:.+`);
+const REJECT_ACTION_RE = new RegExp(`^${ACTION_ID_PREFIXES.REJECT}:.+`);
 
 async function main() {
   console.log('Starting Mastra Slack Agent...');
@@ -15,8 +19,8 @@ async function main() {
 
   // Register Action Handlers
   // We use regex to match dynamic action IDs
-  app.action(/approve:.+/, handleAction);
-  app.action(/reject:.+/, handleAction);
+  app.action(APPROVE_ACTION_RE, handleAction);
+  app.action(REJECT_ACTION_RE, handleAction);
 
   // Start Slack App
   await startSlackApp(app);
